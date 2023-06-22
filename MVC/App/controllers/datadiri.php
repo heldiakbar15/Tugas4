e7161a9f5eec35b83c2f2d3d2c6bec6a811f207d
Heldi Akbar Kosasih <119715615+heldiakbar15@users.noreply.github.com>
2023-06-22 12:20:34 +0700

    Add files via upload

---
+++ MVC/App/controllers/datadiri.php
@@ -0,0 +1,10 @@
+<?php
+
+class dataDiri{
+    public function index(){
+        echo "My Biodata<br>";
+        echo "Nama : Heldi Akbar Kosasih<br>";
+        echo "NIM : D112111026<br>";
+        echo "Kampus : TEDC";
+    }
+}
